3eeda93427f510f7965b18850d8522728ce2ed80
eyao600 <eyao600@PR18.hhs.win.fuhsd.org>
2018-05-02 14:10:59 -0700

    Mario falls normally

--- AnimationDemo/src/Mario.java
+++ AnimationDemo/src/Mario.java
@@ -9,6 +9,7 @@ public class Mario extends Sprite {
 
 	public static final int MARIO_WIDTH = 40;
 	public static final int MARIO_HEIGHT = 60;
+	private int vY;
 	
 
 	public Mario(PImage img, int x, int y) {
@@ -19,7 +20,6 @@ public class Mario extends Sprite {
 	public void walk(int dir) {
 		// WALK!
 		moveByAmount(dir, 0);
-		
 	}
 
 	public void jump() {
@@ -29,14 +29,19 @@ public class Mario extends Sprite {
 
 	public void act(ArrayList<Shape> obstacles) 
 	{
+		vY += 1;
 		boolean freeFall = true;
 		for(Shape s : obstacles)
 		{
 			if(s.contains(this.getCenterX(), this.getMaxY()))
+			{
 				freeFall = false;
+			}
 		}
 		if(freeFall)
-			moveByAmount(0, 1);
+			moveByAmount(0, vY);
+		else
+			vY = 0;
 		// FALL (and stop when a platform is hit)
 	}
 }
