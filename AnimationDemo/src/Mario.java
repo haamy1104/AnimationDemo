e32b5a9c42a55092d91a0997f179eb03c999e078
soberman652 <soberman652@PR19.hhs.win.fuhsd.org>
2018-05-02 13:43:52 -0700

    Coded to walk() method.

--- AnimationDemo/src/Mario.java
+++ AnimationDemo/src/Mario.java
@@ -9,6 +9,7 @@ public class Mario extends Sprite {
 
 	public static final int MARIO_WIDTH = 40;
 	public static final int MARIO_HEIGHT = 60;
+	
 
 	public Mario(PImage img, int x, int y) {
 		super(img, x, y, MARIO_WIDTH, MARIO_HEIGHT);
@@ -17,6 +18,8 @@ public class Mario extends Sprite {
 	// METHODS
 	public void walk(int dir) {
 		// WALK!
+		moveByAmount(20, 0);
+		
 	}
 
 	public void jump() {
